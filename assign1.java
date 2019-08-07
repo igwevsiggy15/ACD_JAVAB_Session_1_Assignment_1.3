b4e89bd439c417a7cdb9bc00a1a5ff27bca1dbb8
igwevsiggy15 <53847461+igwevsiggy15@users.noreply.github.com>
2019-08-07 14:55:59 -0400

    Add files via upload

---
+++ assign1.java
@@ -0,0 +1,12 @@
+
+//Assignment 3
+//& And in Java
+package assign1;
+
+public class assign1 {
+	public static void main(String[] args) {
+	
+	System.out.println("And of 1 & 1 is : "+(0 & 0));
+	
+}
+}
